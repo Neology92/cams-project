1d9771e0d2a3b89b7e6c2a9bf2aa82214445a18a
unknown <kmacioszek99@gmail.com>
2020-02-26 21:57:18 +0100

    changed input box-sizing

--- client/components/ui-elements/Input.js
+++ client/components/ui-elements/Input.js
@@ -38,12 +38,13 @@ const StyledLabel = styled(InputLabel)`
 `;
 const StyledInput = styled(InputBase)`
     .MuiInputBase-input {
+        box-sizing: border-box;
         position: relative;
         border: 1px solid ${({ theme }) => theme.palette.secondary.main};
         background-color: ${({ theme }) => theme.palette.secondary.main};
         border-radius: 4px;
         height: 30px;
-        width: 250px;
+        width: 256px;
         font-size: 14px;
         padding: 0 0 0 4px;
         transition: ${({ theme }) =>
