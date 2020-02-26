714e27d061362df3f9da657f4255eafee1653d85
Neology92 <oskarlegner@gmail.com>
2020-02-26 18:41:38 +0100

    Init styling and remove label

--- client/components/ui-elements/Toggle.js
+++ client/components/ui-elements/Toggle.js
@@ -1,39 +1,59 @@
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { Switch, FormControl } from '@material-ui/core';
 
-import { Switch, FormControl, FormControlLabel } from '@material-ui/core';
-
-const Toggle = ({ value, setValue, label, color }) => {
+const Toggle = ({ value, setValue, color }) => {
     const handleChange = v => {
         setValue(!v);
     };
 
     return (
         <FormControl>
-            <FormControlLabel
-                control={
-                    <Switch
-                        checked={value}
-                        onChange={() => handleChange(value)}
-                        value={value}
-                        color={color}
-                    />
-                }
-                label={label}
+            <IOSSwitch
+                checked={value}
+                onChange={() => handleChange(value)}
+                value={value}
+                color={color}
             />
         </FormControl>
     );
 };
 
+const IOSSwitch = styled(Switch)`
+    && {
+        width: 42px;
+        height: 22px;
+        padding: 0;
+        border-radius: 12px;
+
+        .MuiSwitch {
+            &-switchBase {
+                padding: 1px;
+            }
+
+            &-thumb {
+                border: ${({ theme }) => theme.palette.primary.main} !important;
+                width: 20px;
+                height: 20px;
+                background: ${({ theme }) => theme.palette.common.white};
+            }
+
+            &-track {
+                background-color: ${({ theme }) => theme.palette.primary.main};
+                opacity: 1 !important;
+            }
+        }
+    }
+`;
+
 Toggle.propTypes = {
     color: PropTypes.string,
-    label: PropTypes.string,
     value: PropTypes.bool,
     setValue: PropTypes.func.isRequired,
 };
 
 Toggle.defaultProps = {
     color: 'primary',
-    label: '',
     value: false,
 };
 
