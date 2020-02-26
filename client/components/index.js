53b7c08b5c0b54f841f56786b0cd2a0ef0ef107d
Neology92 <oskarlegner@gmail.com>
2020-02-26 16:59:35 +0100

    Set toggle mechanics

--- client/components/index.js
+++ client/components/index.js
@@ -5,5 +5,6 @@ import Select from './ui-elements/Select';
 import Input from './ui-elements/Input';
 import Radio from './ui-elements/Radio';
 import CheckboxGroup from './ui-elements/CheckboxGroup';
+import Toggle from './ui-elements/Toggle';
 
-export { Navbar, Button, Select, Radio, CheckboxGroup, Input };
+export { Navbar, Button, Select, Radio, CheckboxGroup, Input, Toggle };
