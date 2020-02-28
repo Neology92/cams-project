56938ed756ebbca5e1caebede7ddc14755bb3282
Neology92 <oskarlegner@gmail.com>
2020-02-28 16:36:09 +0100

    Enable icons sizing

--- client/components/Icon.js
+++ client/components/Icon.js
@@ -1,7 +1,24 @@
 import { SvgIcon } from '@material-ui/core';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
-const Icon = ({ ...props }) => (
-    <SvgIcon viewBox="0 0 20 20" fontSize="small" {...props} />
+const Icon = ({ size, ...props }) => (
+    <StyledIcon viewBox="0 0 20 20" fontSize="small" size={size} {...props} />
 );
+
+const StyledIcon = styled(SvgIcon)`
+    && {
+        width: ${({ size }) => size};
+        height: ${({ size }) => size};
+    }
+`;
+
+Icon.propTypes = {
+    size: PropTypes.string,
+};
+
+Icon.defaultProps = {
+    size: '20px',
+};
 
 export default Icon;
