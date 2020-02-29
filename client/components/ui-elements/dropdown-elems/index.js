8432319af272d8467e739d9d2e12d5c384e59ceb
Neology92 <oskarlegner@gmail.com>
2020-02-29 13:43:03 +0100

    Move Dropdown Wrapper to own file

--- client/components/ui-elements/dropdown-elems/index.js
+++ client/components/ui-elements/dropdown-elems/index.js
@@ -1,5 +1,6 @@
 import Divider from './Divider';
 import Subheader from './Subheader';
 import Item from './Item';
+import Menu from './Menu';
 
-export { Divider, Subheader, Item };
+export { Divider, Subheader, Item, Menu };
