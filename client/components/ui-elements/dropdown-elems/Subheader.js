e3e4914d1c0e207d91c1cd03edcd3847b1f94a46
Neology92 <oskarlegner@gmail.com>
2020-02-29 12:39:44 +0100

    Create subheader component

---
+++ client/components/ui-elements/dropdown-elems/Subheader.js
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+import { ListSubheader } from '@material-ui/core';
+
+const Subheader = ({ label }) => {
+    return (
+        <Wrapper>
+            <span>{label}</span>
+        </Wrapper>
+    );
+};
+
+const Wrapper = styled(ListSubheader)`
+    && {
+        padding: 0;
+        text-align: left;
+        height: 25px;
+        position: relative;
+
+        span {
+            position: absolute;
+            top: -15px;
+            height: 12px;
+            min-height: 0;
+        }
+    }
+`;
+
+Subheader.propTypes = {
+    label: PropTypes.string.isRequired,
+};
+
+export default Subheader;
