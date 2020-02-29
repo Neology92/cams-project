8cb97b2782283deda72acfe216ca0ca92ab161df
Neology92 <oskarlegner@gmail.com>
2020-02-29 12:39:16 +0100

    Create divider component

---
+++ client/components/ui-elements/dropdown-elems/Divider.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import MuiDivider from '@material-ui/core/Divider';
+
+const Divider = styled(MuiDivider)`
+    && {
+        margin: 6px 0 5px;
+    }
+`;
+
+export default Divider;
