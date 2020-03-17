e3a7c1e6c43983998e0874d1f5f02d539510207d
Neology92 <oskarlegner@gmail.com>
2020-03-17 17:51:08 +0100

    Fill logout function

--- client/utils/logout.js
+++ client/utils/logout.js
@@ -1,9 +1,26 @@
-import { setInStorage } from './storage';
+import { setInStorage, getFromStorage } from './storage';
 
 export default function() {
-    setInStorage('session_token', '');
+    const sessionToken = getFromStorage('session_token');
 
     // Send logout request to server
+    const response = fetch('/api/logout', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            sessionToken,
+        }),
+    })
+        .then(res => res.json())
+        .then(json => {
+            if (json.success) {
+                setInStorage('session_token', '');
+                window.location.reload();
+            }
+            return json;
+        });
 
-    window.location.reload();
+    return response;
 }
