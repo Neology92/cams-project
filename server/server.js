9bbbf4692c3109fb860e510c281612bfbd719698
Neology92 <oskarlegner@gmail.com>
2020-03-17 17:08:43 +0100

    remove item model and endpoints

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-const items = require('./routes/api/items');
 const users = require('./routes/api/users');
 
 const register = require('./routes/api/register');
@@ -26,7 +25,6 @@ mongoose
     .catch(err => console.log('Error: ', err));
 
 // Use Routes
-app.use('/api/items', items);
 app.use('/api/users', users);
 app.use('/api/register', register);
 app.use('/api/login', login);
