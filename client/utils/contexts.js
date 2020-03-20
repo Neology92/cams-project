31422a6cdd0ea4b99fefb5aad5061f115684dd85
Neology92 <oskarlegner@gmail.com>
2020-03-20 12:47:14 +0100

    Clear files

--- client/utils/contexts.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export const UserContext = React.createContext({
-    sessionToken: null,
-    user: null,
-});
-
-export const AnotherContext = null;
--- server/config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-	mongoURI:
-		'mongodb+srv://Neology92:neo123@dev-cluster-mbgvh.mongodb.net/test?retryWrites=true&w=majority',
-};
