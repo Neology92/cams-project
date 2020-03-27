d098177375563c372e2ef35d44803d627ea826ed
Neology92 <oskarlegner@gmail.com>
2020-03-27 16:58:32 +0100

    Transform func-component to pureComponent

--- client/index.js
+++ client/index.js
@@ -3,19 +3,21 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-function Main() {
-    // React.useEffect(() => {
-    //     const jssStyles = document.querySelector('#jss-server-side');
-    //     if (jssStyles) {
-    //         jssStyles.parentElement.removeChild(jssStyles);
-    //     }
-    // }, []);
+class Main extends React.PureComponent {
+    componentDidMount() {
+        const jssStyles = document.querySelector('#jss-server-side');
+        if (jssStyles) {
+            jssStyles.parentElement.removeChild(jssStyles);
+        }
+    }
 
-    return (
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    );
+    render() {
+        return (
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        );
+    }
 }
 
 ReactDOM.hydrate(<Main />, document.getElementById('app'));
